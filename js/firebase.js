f2ac8a1cbed3813ef2f1f5b3c5f5538cd3efad66
IkerCubillo <i.cubillo@opendeusto.es>
2024-12-10 17:30:08 +0100

    app en firebase añadida

--- js/firebase.js
+++ js/firebase.js
@@ -2,13 +2,15 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "TU_API_KEY",
-    authDomain: "TU_AUTH_DOMAIN",
-    projectId: "TU_PROJECT_ID",
-    storageBucket: "TU_STORAGE_BUCKET",
-    messagingSenderId: "TU_MESSAGING_SENDER_ID",
-    appId: "TU_APP_ID",
-};
+    apiKey: "AIzaSyB2zUrSFgvgRON2DHPg_vGkTY7ZLotEfg8",
+    authDomain: "iw-bi-16.firebaseapp.com",
+    databaseURL: "https://iw-bi-16-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "iw-bi-16",
+    storageBucket: "iw-bi-16.firebasestorage.app",
+    messagingSenderId: "777174914615",
+    appId: "1:777174914615:web:6403390b6647c6c43b91b2",
+    measurementId: "G-KN50E1LCL7"
+  };
 
 const firebaseApp = initializeApp(firebaseConfig);
 export const db = getFirestore(firebaseApp);
